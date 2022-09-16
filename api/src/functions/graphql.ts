c4be1de92009411e8b9ae24aacbb534165202771
Alice Zhao <azhao991@gmail.com>
2022-09-16 12:47:31 -0700

    first commit

---
+++ api/src/functions/graphql.ts
@@ -0,0 +1,22 @@
+// import { generateGraphiQLHeader } from '@redwoodjs/api'
+import { createGraphQLHandler } from '@redwoodjs/graphql-server'
+
+import directives from 'src/directives/**/*.{js,ts}'
+import sdls from 'src/graphql/**/*.sdl.{js,ts}'
+import services from 'src/services/**/*.{js,ts}'
+
+import { db } from 'src/lib/db'
+import generateGraphiQLHeader from 'src/lib/generateGraphiQLHeader'
+import { logger } from 'src/lib/logger'
+
+export const handler = createGraphQLHandler({
+  generateGraphiQLHeader,
+  loggerConfig: { logger, options: {} },
+  directives,
+  sdls,
+  services,
+  onException: () => {
+    // Disconnect from your database with an unhandled exception.
+    db.$disconnect()
+  },
+})
